7af8a150494654404af5ca843ecaf23f9197d95a
kar <kar@gmail.com>
2019-10-22 14:30:25 +0530

    data frame start

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pandas import Series,DataFrame
 
 obj = pd.Series([10,20,30,40,50,60,70,80,90])
@@ -11,7 +12,7 @@ print(obj1)
 print(obj1.values)
 print(obj1.index)
 
-obj2 = pd.Series([15,30,45,60,75,90],index = ['a','b','c','d','e','f'])
+obj2 = pd.Series([12,30,45,60,75,90],index = ['a','b','c','d','e','f'])
 print(obj2)
 print(obj2.index)
 print(obj2[['a','b','c']])
@@ -21,3 +22,15 @@ print(obj2[obj2 == 15])
 print(obj2[obj2 < 30])
 print(obj2[obj2 != 45])
 print(obj2 * 2)
+print(np.exp(obj2))
+print('c' in obj2)
+
+#data contained in a Python dict
+data = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
+state = ['karnataka','tamilnadu','goa','Utah']
+print(pd.Series(data))
+obj3 = pd.Series(data,index=state)
+print(obj3)
+print(pd.isnull(obj3))
+
+
